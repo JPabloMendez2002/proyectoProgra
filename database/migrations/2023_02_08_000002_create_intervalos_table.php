db7cd01f8ed66582a5001fc219063cb5a936d6a2
sitios web <spestechical@gmail.com>
2023-02-08 20:42:01 -0800

    Update 2023_02_08_000002_create_intervalos_table.php

--- database/migrations/2023_02_08_000002_create_intervalos_table.php
+++ database/migrations/2023_02_08_000002_create_intervalos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('Intervalos', function (Blueprint $table) {
             $table->id('IdIntervalo');
-            $table->string('IdIntervalo', 20)->unique();
+            $table->string('Nombre', 20)->unique();
             $table->text('Descripcion');
             $table->timestamps();
         });
